56484fcfeac1f48d2b0c7f9374b7842ab1f67ebd
choin <choin0330@gmail.com>
2024-12-28 21:11:38 +0900

    main app.js, router 추가
    main app.js, router 추가

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from "express";
 // import { db } from "mysql2";
 import dotenv from "dotenv";
 import { config } from "./config.js";
+import mainRouter from "./router/main.js"
 import authRouter from "./router/auth.js";
 import matchRouter from "./router/match.js";
 import stadiumRouter from "./router/stadium.js"
@@ -28,6 +29,7 @@ app.use(
 const port = config.hosting_port.user_back || 8080; // 환경 변수에서 PORT 가져오기
 
 // 테스트용 라우트
+app.use("/", mainRouter)
 app.use("/auth", authRouter);
 app.use("/match", matchRouter);
 app.use("/stadium", stadiumRouter)
